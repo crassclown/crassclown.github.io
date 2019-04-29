3d69b6e5d9d9bbf69fc6ca81c2d3cac7a1c22893
hydra <hendryrpl@gmail.com>
2019-04-29 15:55:56 +0700

    fixed offline experience

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,24 @@
 const staticAssets = [
+    './',
     './css/style.css',
     './js/style.js',
+    './index.html',
+    './images/1-min.png',
+    './images/2-min.png',
+    './images/3-min.png',
+    './fonts/Poppins-Black.otf',
+    './fonts/Poppins-Bold.otf',
+    './fonts/Poppins-Light.otf',
+    './fonts/Poppins-Medium.otf',
+    './fonts/Poppins-Regular.otf',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', async event => {
@@ -32,7 +50,7 @@ async function networkFirst(request) {
         const response = await fetch(request);
         return response;
     } catch (error) {
-        return await cache.match(request);
+        return await caches.match(request);
 
     }
 
