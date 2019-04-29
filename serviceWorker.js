1d118aa029ba5f9cea1d7450ae12e5d824e89a77
hydra <hendryrpl@gmail.com>
2019-04-29 10:09:52 +0700

    preload all assets

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,6 @@
 const staticAssets = [
     './css/style.css',
     './js/style.js',
-    './fonts/Poppins-Black.otf',
-    './fonts/Poppins-Bold.otf',
-    './fonts/Poppins-Light.otf',
-    './fonts/Poppins-Medium.otf',
-    './fonts/Poppins-Regular.otf',
-    './images/1-min.png',
-    './images/2-min.png',
-    './images/3-min.png',
 ];
 
 self.addEventListener('install', async event => {
@@ -35,11 +27,9 @@ async function cacheData(request) {
 }
 
 async function networkFirst(request) {
-    const cache = await caches.open('dynamic-meme');
 
     try {
         const response = await fetch(request);
-        cache.put(request, response.clone());
         return response;
     } catch (error) {
         return await cache.match(request);
